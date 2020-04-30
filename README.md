0df1c2a694567898b4e90b9ef9ba896a2dbf42cb
Rachel How <rachelhow5242@gmail.com>
2020-04-30 20:20:53 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+
+[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/import/project?template=https://github.com/zeit/next.js/tree/canary/examples/with-tailwindcss)
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Nav from '../components/nav'
+
+export default () => (
+  <div>
+    <Nav />
+    <div className="hero">
+      <h1 className="title">Malaysians Who Make</h1>
+    </div>
+  </div>
+)
