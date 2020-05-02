ee00dc9635694ec8a12df38839246a0cf7be6ab8
Rachel How <rachelhow5242@gmail.com>
2020-05-02 23:09:16 +0800

    Filter function

--- components/Filters.jsx
+++ components/Filters.jsx
@@ -1,8 +1,6 @@
-const FilterList = ({ expertiseList, setExpertiseFilter, setPagination }) => {
+const FilterList = ({ expertiseFilter, setExpertiseFilter, setPagination }) => {
 
-    let expertiseFilter = expertiseList.sort();
-
-    const handleChange = (expertise, index) => {
+    const handleChange = (event, expertise, index) => {
       let newExpertiseFilter = [...expertiseFilter];
       expertise.selected = !expertise.selected
       newExpertiseFilter[index] = expertise
@@ -19,7 +17,10 @@ const FilterList = ({ expertiseList, setExpertiseFilter, setPagination }) => {
                 expertiseFilter.map( (expertise, index) => (
                   <div className="mt-1" key={index}>
                     <label>
-                        <input type="checkbox" className="form-checkbox h-4 w-4 mr-2 mb-2" onChange={(e) => handleChange(e, expertise, index)} checked={expertise.selected} />
+                        <input
+                          type="checkbox" className="form-checkbox h-4 w-4 mr-2 mb-2" 
+                          onChange={e => handleChange(e, expertise, index)}
+                          checked={expertise.selected} />
                         <span>{expertise.expertise}</span>
                     </label>
                   </div>
--- components/MakerList.jsx
+++ components/MakerList.jsx
@@ -3,13 +3,13 @@ import Pagination from '../components/Pagination';
 import MakerCard from '../components/MakerCard';
 import FilterList from '../components/Filters';
 
-function MakerList({ makerDirectory, expertiseList, setExpertiseFilter, numberOfPages, pagination, setPagination }) {
+function MakerList({ makerDirectory, expertiseList, expertiseFilter, setExpertiseFilter, numberOfPages, pagination, setPagination }) {
     let makerList = makerDirectory.sort();
 
     return (
       <div className="grid grid-cols-5 gap-8 pl-12 pr-12 pt-10">
           <div>
-              <FilterList expertiseList={expertiseList} setExpertiseFilter={setExpertiseFilter} setPagination={setPagination}/>
+              <FilterList expertiseList={expertiseList} expertiseFilter={expertiseFilter} setExpertiseFilter={setExpertiseFilter} setPagination={setPagination}/>
           </div>
           <div className="col-span-4">
             <div className="grid grid-cols-4 gap-8 row-gap-12">
