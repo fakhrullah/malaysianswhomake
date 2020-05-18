a9b3d19bc18c5915cffa3a9a9ca454a360e2bdfa
Rachel How <rachelhow5242@gmail.com>
2020-05-18 18:08:27 +0800

    Added GA Tracking

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { UA-166954166-1 } from '../lib/gtag';
 
 class MyDocument extends Document {
     
@@ -10,7 +11,24 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=UA-166954166-1`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-166954166-1', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
