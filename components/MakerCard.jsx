cfa95e4d3f22d378f4de88cb7ec42521b6ef48ec
Rachel How <rachelhow5242@gmail.com>
2020-05-20 15:51:01 +0800

    Feature/ adplist tagging

--- components/MakerCard.jsx
+++ components/MakerCard.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-// import ReadMoreAndLess from 'react-read-more-less';
 import ReadMoreAndLess from "../components/ReadMore";
 import Ic_Location from "../src/Ic_Location";
 import Ic_Website from "../src/Ic_Website";
@@ -7,19 +6,21 @@ import Ic_Briefcase from "../src/Ic_Briefcase";
 import Ic_Instagram from "../src/Ic_Instagram";
 import Ic_Twitter from "../src/Ic_Twitter";
 import Ic_LinkedIn from "../src/Ic_LinkedIn";
+import Ic_AdplistTag from "../src/Ic_AdplistTag";
 
 const MakerCard = ({ directory }) => {
   return (
     <div className="max-w-14 sm:max-w-48">
 
-      <div className="h-full">
-        {directory.image ? (
+      <div className="h-full relative">
           <img
             className="w-full h-imglg md:h-imgmd sm:h-imgsm object-cover makerImage"
             src={directory.image}
             alt="img"
           />
-        ) : null}
+          {directory.tag === "adplist" ? 
+            <a className="makerTag" href="https://adplist.org/"><Ic_AdplistTag/></a> 
+            : null}
       </div>
 
       <div className="pt-2 pb-0.75 font-semibold md:text-s sm:text-24">
